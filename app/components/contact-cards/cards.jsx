ec5fe742af97180aaa69549cc29a89a506b75b13
NiklasJaeger <niklas.jaeger@deutschebahn.com>
2024-01-16 14:01:44 +0100

    Added

--- app/components/contact-cards/cards.jsx
+++ app/components/contact-cards/cards.jsx
@@ -1,35 +1,36 @@
-import Link from "next/link"
-import styles from "./card.modules.css"
-import SVGInstagram from "../icons/instagram"
-import SVGEnvelope from "../icons/envelope"
+import Link from "next/link";
+import styles from "./card.modules.css";
+import SVGInstagram from "../icons/instagram";
+import SVGEnvelope from "../icons/envelope";
+import SVGPaypal from "../icons/paypal";
 
 export default function Card({ title, link, icon }) {
+  function renderIcon(icon) {
+    switch (icon) {
+      case "instagram":
+        return <SVGInstagram className="icon" />;
 
-    function renderIcon(icon) {
-        switch (icon) {
-            case "instagram": return <SVGInstagram className="icon" />
+        break;
 
-                break;
+      case "mail":
+        return <SVGEnvelope className="icon" />;
 
-            case "mail": return <SVGEnvelope className="icon" />
-            default:
-                break;
-        }
+      case "paypal":
+        return <SVGPaypal className="icon" />;
+
+      default:
+        break;
     }
+  }
 
-    return (
-        <>
-            <Link href={link}>
-                <div className="card">
-
-                    <h3>
-                        {title}
-                    </h3>
-                    {renderIcon(icon)}
-                </div>
-            </Link>
-
-
-        </>
-    )
+  return (
+    <>
+      <Link href={link} className="card-link">
+        <div className="card">
+          {renderIcon(icon)}
+          <h3>{title}</h3>
+        </div>
+      </Link>
+    </>
+  );
 }
